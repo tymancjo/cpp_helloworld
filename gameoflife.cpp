c0a14e09f23d45dbfba3c5ec05921e3fbab06631
Tomasz Tomanek <tymancjo@pop-os.localdomain>
2021-01-08 21:15:09 +0100

    adding keys and command line support

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,6 +1,8 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
-#include <iostream>
+#include <istream>
+#include <sstream>
+
 
 class Game : public olc::PixelGameEngine
 {
@@ -9,6 +11,8 @@ class Game : public olc::PixelGameEngine
     int *world_next;
     int X;
     int Y;
+    bool is_active = false;
+    olc::Pixel pix = olc::Pixel(33,70,100);
     
 public:
 	Game(int inX, int inY)
@@ -54,6 +58,22 @@ private:
             }
     }
 
+    void restart(){
+
+        for (int x = 0; x < ScreenWidth(); x++){
+            for (int y = 0; y < ScreenHeight(); y++){
+                world[A(x,y)] = 0; 
+                world_next[A(x,y)] = 0; 
+            }
+        }
+
+        for (int x = 1; x < X-1; x++){
+            for (int y = 1; y < Y-1; y++){
+                world[A(x,y)] = rand() % 2;
+            }
+        }
+    }
+
     int sum(int x, int y)
     {
         int local = world[A(x,y)];
@@ -74,18 +94,8 @@ public:
 	{
 		// Called once at the start, so create things here
 
-        for (int x = 0; x < ScreenWidth(); x++){
-            for (int y = 0; y < ScreenHeight(); y++){
-                world[A(x,y)] = 0; 
-                world_next[A(x,y)] = 0; 
-            }
-        }
+        restart();
 
-        for (int x = 1; x < X-1; x++){
-            for (int y = 1; y < Y-1; y++){
-                world[A(x,y)] = rand() % 2;
-            }
-        }
 		return true;
 	}
 
@@ -96,26 +106,59 @@ public:
             for (int y = 0; y < Y; y++)
             {
                 if ( world[A(x,y)] == 0)
-                    Draw(x, y, olc::Pixel(33,70,100));
+                    Draw(x, y, pix); 
                 else
                     Draw(x, y, olc::Pixel(255,255,255));
             }
 
-        next_gen();
+        // checking for user input
+        if (GetKey(olc::Key::SPACE).bPressed) is_active = !is_active;
+        if (GetKey(olc::Key::R).bPressed) restart();
+        if (GetKey(olc::Key::C).bPressed) 
+        {
+            int rclr = rand() % 150;
+            pix = olc::Pixel(rclr / 3, rclr * 2/3, rclr);
+        } 
 
-		return true;
+        if (is_active)
+            next_gen();
+
+        return true;
 	}
 };
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    const int d = 4;
-    const int iW = 1920 / d;
-    const int iH = 1080 / d;
+    int d = 3;
+    int iW = 1920 / d;
+    int iH = 1080 / d;
+    
+    if (argc > 3){
+        // reading the line parameters and converting to int
+        // based on the: https://stackoverflow.com/a/35206575/14836163
+        std::istringstream iss( argv[1] );
+        int val;
+        if ((iss >> val) && iss.eof()){
+            iW = val;
+
+            std::istringstream iss2( argv[2] );
+            if ((iss2 >> val) && iss2.eof()){
+                iH = val;
+
+                std::istringstream iss3( argv[3] );
+                if ((iss3 >> val) && iss3.eof()){
+                    d = val;
+                    iW = iW / d;
+                    iH = iH / d;
+                }
+            }
+        }
+    }
+
 
     int size = iW * iH;
-    std::cout << size;
+    std::cout << "[INFO] World size: " << size << std::endl;
 
 	Game life(iW,iH);
 	if (life.Construct(iW, iH, d, d))
