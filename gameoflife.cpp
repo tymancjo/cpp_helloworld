69f87a2ac56b5ff2c109776c79dc14772c8bfdf4
Tomasz Tomanek <tymancjo@pop-os.localdomain>
2021-01-10 23:43:45 +0100

    added spash screen

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,8 +1,29 @@
+/*
+	License (OLC-3)
+	~~~~~~~~~~~~~~~
+
+	Copyright 2018 - 2020 OneLoneCoder.com
+
+	Redistribution and use in source and binary forms, with or without modification,
+	are permitted provided that the following conditions are met:
+
+	1. Redistributions or derivations of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions or derivative works in binary form must reproduce the above
+	copyright notice. This list of conditions and the following	disclaimer must be
+	reproduced in the documentation and/or other materials provided with the distribution.
+
+	3. Neither the name of the copyright holder nor the names of its contributors may
+	be used to endorse or promote products derived from this software without specific
+	prior written permission.
+*/
+
+
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include <istream>
 #include <sstream>
-
 
 class Game : public olc::PixelGameEngine
 {
@@ -13,6 +34,7 @@ class Game : public olc::PixelGameEngine
     int Y;
     bool is_active = false;
     olc::Pixel pix = olc::Pixel(33,70,100);
+    bool startup = true;
     
 public:
 	Game(int inX, int inY)
@@ -93,42 +115,73 @@ public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
-
         restart();
-
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-        bool step = false;
-		// called once per frame
-        for (int x = 0; x < X; x++)
-            for (int y = 0; y < Y; y++)
-            {
-                if ( world[A(x,y)] == 0)
-                    Draw(x, y, pix); 
-                else
-                    Draw(x, y, olc::Pixel(255,255,255));
+
+        if ( startup ){
+            if (GetKey(olc::Key::SPACE).bPressed) startup = false;
+
+                std::vector <std::string> main_text;
+                uint32_t text_size = 1 + X / 300;
+
+                main_text.push_back("Game of Life [C++]");
+                main_text.push_back("by Tymancjo");
+                main_text.push_back("powered by:");
+                main_text.push_back("olcPixelGameEngine");
+                main_text.push_back("keys:");
+                main_text.push_back("SPACE - pause/resume");
+                main_text.push_back("S - single generation");
+                main_text.push_back("R - restart");
+                main_text.push_back("C - change back color");
+                main_text.push_back(" ");
+                main_text.push_back(" ");
+                main_text.push_back("World size: ");
+                main_text.push_back(std::to_string( X * Y ));
+                main_text.push_back("Press SPACE...");
+            
+            
+            for (int k = 0; k < main_text.size(); k++){
+                std::string this_line = main_text[k];
+                int txtX = (X - 7 * text_size * this_line.length()) / 2;
+
+                DrawString(txtX, 20 + k * 12 *text_size, this_line, olc::WHITE, text_size);
             }
 
-        // checking for user input
-        if (GetKey(olc::Key::SPACE).bPressed) is_active = !is_active;
-        if (GetKey(olc::Key::R).bPressed) restart();
-        if (GetKey(olc::Key::S).bPressed) {
-            step = true; 
-            is_active = false;
-        }
-        if (GetKey(olc::Key::C).bPressed) 
-        {
-            int rclr = rand() % 150;
-            pix = olc::Pixel(rclr / 3, rclr * 2/3, rclr);
-        } 
+        } else {
+            bool step = false;
+            // called once per frame
+            for (int x = 0; x < X; x++)
+                for (int y = 0; y < Y; y++)
+                {
+                    if ( world[A(x,y)] == 0)
+                        Draw(x, y, pix); 
+                    else
+                        Draw(x, y, olc::Pixel(255,255,255));
+                }
 
-        if (is_active || step){
-            next_gen();
-            step = false;
-        }
+            // checking for user input
+            if (GetKey(olc::Key::SPACE).bPressed) is_active = !is_active;
+            if (GetKey(olc::Key::R).bPressed) restart();
+            if (GetKey(olc::Key::S).bPressed) {
+                step = true; 
+                is_active = false;
+            }
+            if (GetKey(olc::Key::C).bPressed) 
+            {
+                int rc1 = rand() % 150;
+                int rc2 = rand() % 150;
+                int rc3 = rand() % 150;
+                pix = olc::Pixel(rc1 / 3, rc2 * 2/3, rc3);
+            } 
+
+            if (is_active || step){
+                next_gen();
+                step = false;
+        }}
 
         return true;
 	}
